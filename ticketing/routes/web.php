46e2bc09c6aef969318e02c26c97232af9fb173d
John Anthony Rico <johnrico364@gmail.com>
2025-08-02 00:24:14 +0800

    Add necessary UI

--- ticketing/routes/web.php
+++ ticketing/routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.login');
+})->name('login');
+
+Route::get('signup', function () {
+    return view('pages.signup');
+})->name('signup');
+
+Route::get('home', function () {
+    return view('pages.home');
+})->name('home');
+
